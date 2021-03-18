44b9cb1744b213fa0e910a792aa95b0c57a213b9
Raymundo Solis <pepperray01@gmail.com>
2021-03-17 18:16:25 -0600

    [Create] new project and product component

---
+++ src/app/components/product.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from '../product.model';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+})
+export class ProductComponent {
+  @Input() product: Product;
+  @Output() productClick: EventEmitter<any> = new EventEmitter();
+
+  addCart() {
+    console.log('Añadir al carrito');
+    this.productClick.emit(this.product.id);
+  }
+}

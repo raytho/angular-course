c3ed8f9b3468f6face0b910036bcad4f66b48a32
Raymundo Solis <pepperray01@gmail.com>
2021-03-22 10:41:51 -0600

    [Adding] Styles in products

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -4,6 +4,7 @@ import { Product } from '../product.model';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent {
   @Input() product!: Product;
